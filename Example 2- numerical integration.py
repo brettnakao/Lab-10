6657b823246eebb54f63ce8ff6f21f10b4806c98
brettnakao <bnakao@lion.lmu.edu>
2024-04-15 16:55:44 -0700

    example 2 finished

--- Example 2- numerical integration.py
+++ Example 2- numerical integration.py
@@ -6,22 +6,29 @@ Created on Mon Apr 15 14:39:42 2024
 @author: brettnakao
 """
 
+# Import libraries
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import quad
 
-# Integrate function from 0 to 2
-def f(x): return x**2
-integral, _ = quad(f, 0, 2)
-integral_check = 2**3/3
+# Integrate function f(x) from 0 to 2
+def f(x): return x**2           # Define function
+integral, _ = quad(f, 0, 2)     # Integrate function
+integral_check = 2**3/3         # Compare to exact value of integration
 
-# Evaluate integral from 0 to 5
-def g(x): return np.exp((-x**2)/2)
-integral_2, _ = quad(g, 0, 5)
+# Evaluate integral g(x) from 0 to 5
+def g(x): return np.exp((-x**2)/2)          # Define function
+upper_limit = np.linspace(0, 5, 20)         # Define upper limit
+exp_integral = np.zeros(upper_limit.size)   # Create array for integral values
+for i in range(upper_limit.size):
+    exp_integral[i], _ = quad(g, 0, upper_limit[i])
 # Plot the results
 plt.figure()
-plt.plot(integral_2)
+plt.plot(upper_limit, exp_integral)
+plt.xlable('x')
+plt.ylable('integral')
+plt.title('Integral of g(x)')
 
 # Evaluate infinite limits
 integral_inf, _ = quad(g, -np.inf, np.inf)
-integral_inf_check = np.sqrt(2*np.pi)
+integral_inf_check = np.sqrt(2*np.pi)       # Compare to exact value of integration
